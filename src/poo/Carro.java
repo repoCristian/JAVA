49d7f74b8c3f618a02bf86ffb1ca97c4d14b4435
repoCristian <cdtpemp@gmail.com>
2024-04-15 17:00:20 -0500

    001 class car creation

---
+++ src/poo/Carro.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Carro {
+	int largo;
+	int ancho;
+	int ruedas;
+	int cilindraje;
+	int peso;
+	
+	public Carro() {
+		largo = 20;
+		ancho = 10;
+		ruedas = 4;
+		cilindraje = 2000;
+		peso = 1000;
+	}
+}
---
+++ src/poo/UsoCarro.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class UsoCarro {
+
+	public static void main(String[] args) {
+		
+		Carro carro1 = new Carro();
+		System.out.println("ancho: " + carro1.ancho );
+	}
+
+}
